d028d440c70dcba20c58749dfe716f38fcf060fc
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2025-06-09 18:39:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,10 @@ from strategy import XAUTradingStrategy
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Убедимся, что кэш yfinance не используется
-os.environ["PY_YFINANCE_DISABLE_CACHE"] = "1"
-
 def run_background_monitor():
-    """Основной цикл мониторинга"""
-    # Инициализация модели и стратегии
     xau_model = XAUModel()
     trading_strategy = XAUTradingStrategy(xau_model)
 
-    # Загрузка начальных данных
-    daily_data, fifteen_min_data = get_xau_data()
-    if daily_data.empty or fifteen_min_data.empty:
-        logging.error("Не удалось загрузить данные при запуске")
-        return
-
-    # Проверка наличия токена и chat_id
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     chat_id = os.getenv('CHAT_ID')
 
@@ -35,12 +23,10 @@ def run_background_monitor():
             # Инициализация Telegram-бота
             telegram_handler = TelegramHandler(telegram_token, chat_id, trading_strategy)
             telegram_handler.start()
-            logging.info("Telegram-бот успешно запущен")
+            logging.info("Telegram-бот запущен")
 
-            # Основной цикл проверки сигналов
             while True:
                 try:
-                    logging.info("Проверка новых данных...")
                     daily_data, fifteen_min_data = get_xau_data()
 
                     if not daily_data.empty and not fifteen_min_data.empty:
@@ -63,7 +49,6 @@ def run_background_monitor():
             logging.error(f"Критическая ошибка: {e}")
     else:
         logging.error("TELEGRAM_TOKEN или CHAT_ID не установлены")
-
 
 if __name__ == '__main__':
     run_background_monitor()
